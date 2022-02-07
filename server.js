a27c1baf8610dac9da14c0153b09df697c321561
yogesh kumar <yogesh.kumar@remotestate.com>
2022-02-07 14:49:19 +0530

    second commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`${process.env.HEY} World`);
+  res.send(`hello World`);
 });
 
 app.listen(PORT, HOST);
